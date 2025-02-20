520a7be47392f19f545970f054e059b2dd23b8cd
Amulya656 <23211a0503@bvrit.ac.in>
2025-02-20 10:32:33 +0530

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,12 @@
+<div id="matchResults"></div>
+
+    <h2>Chat</h2>
+    <div id="chatWindow"></div>
+    <input id="messageInput" placeholder="Type a message">
+    <button id="sendMessage">Send Message</button>
+
+    <h2>Audio/Video Call</h2>
+    <button id="audioCall">Start Audio Call</button>
+    <button id="videoCall">Start Video Call</button>
+
+    <script src="app.js"></script>
